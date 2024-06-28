5066ccd694e9950784b895230eee81ba941c6244
Niko Kozak <nikokozak@gmail.com>
2024-06-28 19:06:48 -0400

    default port values for mielesearch start script

--- scripts/start_meili.sh
+++ scripts/start_meili.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -p 7700:$MEILISEARCH_PORT getmeili/meilisearch:latest meilisearch --master-key=$MEILISEARCH_API_KEY
+docker run -it --rm -p 7700:${MEILISEARCH_PORT:=6969} getmeili/meilisearch:latest meilisearch --master-key=$MEILISEARCH_API_KEY
